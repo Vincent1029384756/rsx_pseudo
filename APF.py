d3a0d86c4699573cd0d3db1b83ecbd70a94a8dea
Vincent1029384756 <wengu563@gmail.com>
2024-07-27 15:08:27 -0400

    initial commit

---
+++ APF.py
@@ -0,0 +1,63 @@
+import math
+import numpy as np
+
+class APF():
+
+    def _init_(self, start, goal, obstacles):
+        
+        '''
+        start: 3X1 np.array
+        goal: 3X1 np.array
+        obstacles: list of 3X1 np.arrays?
+        '''
+
+        #APF parameters
+        '''
+        zeta & d: APF parameters for calculating F_att and F_rep
+        time_step: used for calculating new position
+        '''
+        self.zeta = 1.0
+        self.time_step = 0.1 #sec
+        self.d = 1.0
+        self.v = np.array([0.0, 0.0, 0.0])
+        self.k = 0
+        self.cur_pos = start
+        self.goal = goal
+        self.obstacles = obstacles
+
+
+    def change_zeta(self, zeta):
+        self.zeta = zeta
+    
+    def change_time_step(self, time_step):
+        self.time_step = time_step
+    
+    def change_d(self, d):
+        self.d = d
+    
+    def move(self):
+        self.cur_pos = self.cur_pos + self.v*self.time_step
+    
+    def calc_F_att(self):
+        #calculate attractive force
+        distance = np.linalg.norm(self.cur_pos, self.goal)
+
+        if distance <= self.d:
+            F_att = -self.zeta(self.cur_pos - self.goal)
+
+        elif distance >= self.d:
+            F_att = -self.d*self.zeta*(self.cur_pos - self.goal)
+
+        return F_att
+    
+    def calc_rho(self):
+        rho = 
+    
+    def cal_F_rep(self):
+        #calculate repulsive force
+        distance = np.linalg.norm(self.cur_pos, self.goal)
+
+        if distance >= self.d:
+            F_rep = 0
+
+        elif distance <= self.d:
